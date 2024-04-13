df9df8c2f499d77bd29570d77260fd925915f052
ml1zinc <ip.eclizine@gmail.com>
2024-04-13 15:09:35 +0300

    init

---
+++ destroy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SERVICE="$1"
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+DEPLOY_FILE="docker-compose.${SERVICE}.yml"
+
+if [ ! -f $DEPLOY_FILE ]; then
+    echo "No deploy file(${DEPLOY_FILE}) for service: \"${SERVICE}\""
+
+else
+    echo "Start: \"${SERVICE}\" from ${DEPLOY_FILE}"
+fi
+
+
+docker-compose -p homelab_${SERVICE} -f ${DEPLOY_FILE} down
+

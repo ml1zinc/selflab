7eec4ce645902851e4f12f71f546d4165b3819be
ml1zinc <ip.eclizine@gmail.com>
2025-01-18 20:30:22 +0200

    * move to single-db instance

--- setup.py
+++ setup.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
 
+from collections.abc import Callable
 import bcrypt
 import os
 from string import Template
 from pathlib import Path
+from argparse import ArgumentParser
+from functools import wraps
 
 ROOT_DIR = Path(__file__).parent
 SALT = bcrypt.gensalt()
 UID = 1000
 
+SERVICES: dict[str, list[Callable]] = {'all': []}
+DBS: dict[str, list[Callable]] = {'all': []}
+
+def service(service_name: str, service_dict: dict = SERVICES):
+    def decorator(func):
+        @wraps(func)
+        def wrapper():
+            print(f'Setup {service_name}')
+            func()
+            print(f'Complete {service_name}')
+
+        services = service_dict.setdefault(service_name, [])
+        services.append(wrapper)
+
+        service_dict['all'].append(wrapper)
+
+        return wrapper
+    return decorator
 
 def load_env(path: Path) -> dict:
     env = {}
@@ -87,13 +108,18 @@ def mkdir(file: str | Path, uid=UID, gid=UID, mode=0o755):
 def create_password(password: str) -> str:
     return bcrypt.hashpw(password.encode("utf-8"), SALT).decode("utf-8")
 
+@service('postgres')
+def setup_postgres(env):
+    # postgres
+    mkdir("postgres/data")
+    mkdir("postgres/init")
+    copy("postgres/init/services.sql", env)
 
-def main():
-    env_path = ROOT_DIR / '.env'
-    env = load_env(env_path)
-    gen_sample_env(env_path)
 
-    env['T_ADMIN_PASSWORD_HASH'] = create_password(env['T_ADMIN_PASSWORD'])
+@service('traefik')
+def setup_traefik(env):
+    # traefik
+    env["T_ADMIN_PASSWORD_HASH"] = create_password(env["T_ADMIN_PASSWORD"])
 
     mkdir("traefik/config")
     touch("traefik/acme.json", mode=0o600)
@@ -104,6 +130,8 @@ def main():
     copy("traefik/config/tls.yml", env, is_template=False)
 
 
+@service('matrix')
+def setup_matrix(env):
     # matrix
     mkdir("matrix/synapse")
     copy("matrix/synapse/homeserver.yaml", env)
@@ -115,8 +143,46 @@ def main():
     copy("matrix/nginx/www/.well-known/matrix/server", env)
 
 
+@service('trilium')
+def setup_trilium(env):
+    # trilium
+    mkdir("trilium/trilium-data")
+
+
+@service('qbittorrent')
+def setup_qbittorrent(env):
     # qbittorrent
     mkdir("qbittorrent/config")
 
+
+@service('gitea')
+def setup_gitea(env):
+    pass
+
+
+def main():
+    env_path = ROOT_DIR / ".env"
+    env = load_env(env_path)
+
+
+    parser = ArgumentParser(prog='Docker setup')
+
+    parser.add_argument('--setup', choices=list(SERVICES.keys()))
+    parser.add_argument('--setup_db', choices=list(DBS.keys()))
+    parser.add_argument('--gen_sample_env', action='store_true')
+
+    args = parser.parse_args()
+
+
+    if args.gen_sample_env:
+        gen_sample_env(env_path)
+
+    service_to_setup = args.setup
+    if service_to_setup:
+        for setup_func in SERVICES[service_to_setup]:
+            setup_func(env)
+
+
 if __name__ == "__main__":
     main()
+
